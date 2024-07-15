87c7e7e508f2a3ded7bf3c0f2af0aac09b589b78
Ruxyen <rubenteruel2004@gmail.com>
2024-07-15 22:50:00 +0200

    .

---
+++ index.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>CF Reus</title>
+    <link rel="icon" href="images/cfreus.png" type="image/x-icon">
+    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+
+</head>
+
+<body>
+    <header>
+        <div class="main-nav">
+            <div class="nav-left">
+                <div style="margin-right:50px;" class="dropdown">
+                    <a href="#" class="language" data-lang="es">ES</a>
+                    <div class="dropdown-content">
+                        <a href="#" class="language" data-lang="cat">CAT</a>
+                        <a href="#" class="language" data-lang="eng">ENG</a>
+                    </div>
+                </div>
+                
+                <a href="https://www.instagram.com" target="_blank"><i class="fab fa-instagram"></i></a>
+                <a href="https://www.twitter.com" target="_blank"><i class="fab fa-twitter"></i></a>
+                <a href="https://www.tiktok.com" target="_blank"><i class="fab fa-tiktok"></i></a>
+                <a href="https://www.facebook.com" target="_blank"><i class="fab fa-facebook"></i></a>
+                
+            </div>
+            <div class="nav-center">
+                <a href="index.php"><img src="images/cfreus.png" alt="Escudo del Club" class="club-logo"></a>
+            </div>
+            <div class="nav-right">
+                <a href="index.php">Inicio</a>
+                <div class="dropdown">
+                    <a href="#">Equipo</a>
+                    <div class="dropdown-content">
+                        <a href="plantilla.php">Plantilla</a>
+                        <a href="calendario.php">Calendario</a>
+                        <a href="#clasificacion">Clasificación</a>
+                        <a href="#equipacion">Equipación</a>
+                    </div>
+                </div>
+                <a href="#noticias">Noticias</a>
+                <a href="#historia">Club</a>
+                <a href="#campo">Campo</a>
+                <a href="#contacto">Contacto</a>
+            </div>
+        </div>
+    </header>
+
+    <main>
+        <!-- Portada -->
+        <section id="portada" class="hero-section">
+            <div style="margin-bottom:540px;" class="hero-content">
+                <h1>REUS DE TOTA LA VIDA</h1>
+                <a class="plantilla-btn" style="text-decoration:none;" href="plantilla.php">PLANTILLA</a>
+                <p class="years">Desde 2022</p>
+                <a alt="Hacer click aquí" href="https://maps.app.goo.gl/fYCR9h6G5bLijNRu5"><i class="fas fa-map-marker-alt"></i> Camp de futbol Les Borges </a>
+            </div><br><br>
+            <p style="position:relative; top:-55px;" class="lema">Oportunidad de dar un salto internacional a Arabia</p>
+        </section>
+
+        <!-- Sección de mapa y ubicación -->
+        <section class="mapa-ubicacion-section">
+            <div style="margin-top:75px;" class="ubicacion-texto">
+                <h1>Dónde nos encontramos</h1><br><br>
+                <p style="text-align:center">Nuestro campo de fútbol está ubicado en el corazón de Reus, en el Camp de futbol Les Borges. <br><br>Ven y disfruta de nuestros partidos en un ambiente familiar y apasionado.</p>
+            </div>
+            <div class="mapa">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d6006.095565216915!2d1.0144997!3d41.177125!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12a14f6a434a4a6d%3A0xafcc2742f134e0c9!2sCamp%20de%20futbol%20Les%20Borges!5e0!3m2!1sfr!2ses!4v1720693797516!5m2!1sfr!2ses" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+        </section>
+    </main>
+    
+    <footer class="footer">
+        <div class="footer-content">
+            <p>&copy; 2024. CF Reus. Todos los derechos reservados.</p>
+        </div>
+    </footer>
+   
+</body>
+
+</html>
